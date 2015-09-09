1861382bc1af58a4bcd4fc78760b654884966559
PangFeng <pangfeng0415@gmail.com>
2015-09-09 18:29:29 +0800

    WeatherActivity.java

--- gen/com/example/coolweather/R.java
+++ gen/com/example/coolweather/R.java
@@ -14,12 +14,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int city_name=0x7f070002;
+        public static final int current_date=0x7f070005;
         public static final int list_view=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int menu_settings=0x7f070009;
+        public static final int publish_text=0x7f070003;
+        public static final int temp1=0x7f070007;
+        public static final int temp2=0x7f070008;
         public static final int title_text=0x7f070000;
+        public static final int weather_desp=0x7f070006;
+        public static final int weather_info_layout=0x7f070004;
     }
     public static final class layout {
         public static final int choose=0x7f030000;
+        public static final int weather_layout=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
